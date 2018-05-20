e3d9a4a56854bab2c04de7ce5633c0582fe5c947
Bryan Wyatt <brwyatt@gmail.com>
2018-05-20 12:49:53 -0700

    Use config file from S3

--- smartthings_logging/smartthings.py
+++ smartthings_logging/smartthings.py
@@ -1,8 +1,13 @@
+import json
+
 from smartthings_cli import smartthings_cli
+
+from .s3 import getConfig
 
 
 def getData(device_type):
-    config = smartthings_cli.load_config()
+    config = json.loads(getConfig('smartthings-logging-config',
+                                  'smartthings_cli.json'))
 
     access_token = config['access_token']
     endpoint_base_url, endpoint_url = smartthings_cli.get_endpoint_url(
