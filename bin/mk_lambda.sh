636efd361aabac5b70b4060b4baa508465e96daf
Bryan Wyatt <brwyatt@gmail.com>
2019-02-24 11:48:31 -0800

    Ensure .so file are included, too
    Since some of these are built extensions, we kinda need those, too.

--- bin/mk_lambda.sh
+++ bin/mk_lambda.sh
@@ -43,20 +43,20 @@ mkdir -p "${build_dir}/functions" "${build_dir}/layers"
 # Process modules dir
 echo "Adding package modules..."
 cd "${modules_dir}"
-zip -r "${lambda_complete_zip}" * -i "*.py"
+zip -r "${lambda_complete_zip}" * -i "*.py" "*.so"
 cd "${lib_tmp_path}"
 ln -s "${modules_dir}" "${lib_tmp_name}"
-zip -r "${lambda_layer_zip}" "${lib_tmp_name}" -i "*.py"
+zip -r "${lambda_layer_zip}" "${lib_tmp_name}" -i "*.py" "*.so"
 for file in $(ls "${lib_tmp_name}" | filter_dirs); do
-    zip -r "${build_dir}/layers/${file}.zip" "${lib_tmp_name}/${file}" -i "*.py"
+    zip -r "${build_dir}/layers/${file}.zip" "${lib_tmp_name}/${file}" -i "*.py" "*.so"
 done
 rm -f "${lib_tmp_name}"
 
 # Process Lambda dir
 echo "Adding Lambda functions..."
 cd "${lambda_dir}"
-zip -r "${lambda_complete_zip}" * -i "*.py"
-zip -r "${lambda_function_zip}" * -i "*.py"
+zip -r "${lambda_complete_zip}" * -i "*.py" "*.so"
+zip -r "${lambda_function_zip}" * -i "*.py" "*.so"
 for file in $(ls); do
     zip -r "${build_dir}/functions/${file:0:-3}.zip" "${file}"
 done
@@ -68,17 +68,17 @@ mkdir -p "${dependencies_dir}"
 rm -rf "${dependencies_dir}"/*
 grep -ive '^boto3[=<>]' requirements.txt | pip3 install -r /dev/stdin --target "${dependencies_dir}"
 cd "${dependencies_dir}"
-zip -r "${lambda_complete_zip}" * -i "*.py"
+zip -r "${lambda_complete_zip}" * -i "*.py" "*.so"
 cd "${lib_tmp_path}"
 ln -s "${dependencies_dir}" "${lib_tmp_name}"
-zip -r "${lambda_layer_zip}" * -i "*.py"
+zip -r "${lambda_layer_zip}" * -i "*.py" "*.so"
 for file in $(ls "${lib_tmp_name}" | filter_dirs); do
     if [[ "${file}" == *".py" ]]; then
         filename="${file:0:-3}"
     else
         filename="${file}"
     fi
-    zip -r "${build_dir}/layers/${filename}.zip" ""${lib_tmp_name}"/${file}" -i "*.py"
+    zip -r "${build_dir}/layers/${filename}.zip" ""${lib_tmp_name}"/${file}" -i "*.py" "*.so"
 done
 rm -f "${lib_tmp_name}"
 
